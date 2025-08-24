fd3dfcdc6d8fdf76c9602e5056034580fe7bd70e
albertkingdom <albertkingdom@gmail.com>
2025-08-24 22:54:40 +0800

    Enhance retrieval process by integrating MultiQueryRetriever for optimized querying

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from langchain_community.vectorstores import Qdrant
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain import hub
 from langchain.schema import HumanMessage, SystemMessage
+from langchain.retrievers.multi_query import MultiQueryRetriever
+import langchain
+
+langchain.debug = True
 
 
 # ====== API Key is loaded from .env file by docker-compose ======
@@ -34,8 +38,7 @@ vectorstore = Qdrant.from_documents(
     collection_name="rag_demo"
 )
 
-# ====== Step 3: 檢索 + 生成 ======
-retriever = vectorstore.as_retriever()
+# ====== Step 3: 檢索 + 生成 (優化: MultiQueryRetriever) ======
 # prompt = hub.pull("rlm/rag-prompt")  # 從 LangChain Hub 取得現成 prompt
 BASE_URL="https://litellm-ekkks8gsocw.dgx-coolify.apmic.ai"
 MODEL="gemma-3-12b-it"
@@ -46,11 +49,17 @@ MODEL="gemma-3-12b-it"
 # 使用OpenAI
 llm = ChatOpenAI(model_name="gpt-4o", temperature=0)
 
+# 1. 使用 MultiQueryRetriever 優化檢索， LLM 根據使用者的原始問題，生成多個不同角度、不同措辭的相似問題。然後用這些問題一起去資料庫中搜尋
+retriever = MultiQueryRetriever.from_llm(
+    retriever=vectorstore.as_retriever(),
+    llm=llm
+)
+
 # 範例查詢
 question = "此網頁的主要內容是什麼？"
 docs = retriever.invoke(question)
 combined = "\n\n".join(d.page_content for d in docs)
-print(combined + "\n\n")
+# print(combined + "\n\n")
 messages = [
     SystemMessage(content="You are a helpful assistant."),
     HumanMessage(content=f"Context:\n{combined}\n\nQuestion: {question}"),
